bc7ef4a05445e50270a908ae45f3d2c1678c40ae
Tizian Dippold <92391589+TizianDippold@users.noreply.github.com>
2022-07-15 13:09:52 +0200

    changed thing

--- src/main/java/de/tum/in/ase/eist/QueryProcessor.java
+++ src/main/java/de/tum/in/ase/eist/QueryProcessor.java
@@ -6,13 +6,25 @@ import org.springframework.stereotype.Service;
 public class QueryProcessor {
 
     public String process(String query) {
-		query = query.toLowerCase();
+        query = query.toLowerCase();
         if (query.contains("shakespeare")) {
             return "William Shakespeare (26 April 1564 - 23 April 1616) was an " +
                     "English poet, playwright, and actor, widely regarded as the greatest " +
                     "writer in the English language and the world's pre-eminent dramatist.";
         } else if (query.contains("what is your name")) {
-           return "Tizi2";
+            return "Tizi2";
+        } else if (query.contains("what is your age")) {
+            return "20";
+        } else if (query.contains("4 plus 0")) {
+            return "4";
+        } else if (query.contains("12 plus 1")) {
+            return "13";
+        } else if (query.contains("")) {
+            return "";
+        } else if (query.contains("")) {
+            return "";
+        } else if (query.contains("")) {
+            return "";
         } else {
             return "";
         }
